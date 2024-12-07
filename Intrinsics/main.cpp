5add35bbd1dbce1744378b2820660203f1b83753
prostatus <mel.sirvania@gmail.com>
2024-12-07 23:40:23 +0300

    Improved non-aligned add method

--- Intrinsics/main.cpp
+++ Intrinsics/main.cpp
@@ -1,11 +1,11 @@
-#include<immintrin.h>
+#include<intrin.h>
 #include<malloc.h>
 #include<iostream>
 #include<chrono>
 
 using namespace std;
 
-const int VEC_SIZE = 80000000;
+const int VEC_SIZE = 40000011;
 
 void addVectors(double* v1, double* v2, double* out, int size) {
 	int i;
@@ -16,6 +16,14 @@ void addVectors(double* v1, double* v2, double* out, int size) {
 		__m256d x2 = _mm256_add_pd(x0, x1);
 
 		_mm256_storeu_pd(&out[i], x2);
+	}
+	for (; i < (size/2)*2; i+=2) {
+		__m128d x0 = _mm_loadu_pd(&v1[i]);
+		__m128d x1 = _mm_loadu_pd(&v2[i]);
+
+		__m128d x2 = _mm_add_pd(x0, x1);
+
+		_mm_storeu_pd(&out[i], x2);
 	}
 	for (; i < size; i++) {
 		out[i] = v1[i] + v2[i];
@@ -36,5 +44,9 @@ int main() {
 	auto res = chrono::duration_cast<chrono::milliseconds>(stop - start);
 
 	cout << res.count();
+
+	delete[] a;
+	delete[] b;
+	delete[] c;
 
 }
