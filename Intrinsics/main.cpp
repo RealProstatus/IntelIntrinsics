eb56ac11af0efc07d4c9a0d6d7edfea5d8fd3189
prostatus <mel.sirvania@gmail.com>
2024-12-07 23:29:46 +0300

    Non-aligned add released

--- Intrinsics/main.cpp
+++ Intrinsics/main.cpp
@@ -1,5 +1,40 @@
-#include"intrin.h"
+#include<immintrin.h>
+#include<malloc.h>
+#include<iostream>
+#include<chrono>
+
+using namespace std;
+
+const int VEC_SIZE = 80000000;
+
+void addVectors(double* v1, double* v2, double* out, int size) {
+	int i;
+	for (i = 0; i < (size/4)*4; i += 4) {
+		__m256d x0 = _mm256_loadu_pd(&v1[i]);
+		__m256d x1 = _mm256_loadu_pd(&v2[i]);
+
+		__m256d x2 = _mm256_add_pd(x0, x1);
+
+		_mm256_storeu_pd(&out[i], x2);
+	}
+	for (; i < size; i++) {
+		out[i] = v1[i] + v2[i];
+	}
+}
 
 int main() {
-	return 0;
+	double* a, * b, * c;
+
+	a = new double[VEC_SIZE];
+	b = new double[VEC_SIZE];
+	c = new double[VEC_SIZE];
+	
+	auto start = chrono::high_resolution_clock::now();
+	addVectors(a, b, c, VEC_SIZE);
+	auto stop = chrono::high_resolution_clock::now();
+
+	auto res = chrono::duration_cast<chrono::milliseconds>(stop - start);
+
+	cout << res.count();
+
 }
