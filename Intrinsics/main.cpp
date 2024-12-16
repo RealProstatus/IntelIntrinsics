e1e207917e95f659891db1ad46d1ff97984a77b5
prostatus <mel.sirvania@gmail.com>
2024-12-16 10:52:55 +0300

    mulVector added

--- Intrinsics/main.cpp
+++ Intrinsics/main.cpp
@@ -5,37 +5,70 @@
 
 using namespace std;
 
-const int VEC_SIZE = 40000011;
+const int VEC_SIZE = 80000011;
 
-void addVectors(double* v1, double* v2, double* out, int size) {
+void addVectors(const double* v1,const double* v2, double* out, int size) {
 	int i;
+#pragma ivdep
 	for (i = 0; i < (size/4)*4; i += 4) {
-		__m256d x0 = _mm256_loadu_pd(&v1[i]);
-		__m256d x1 = _mm256_loadu_pd(&v2[i]);
+		__m256d x0 = _mm256_load_pd(&v1[i]);
+		__m256d x1 = _mm256_load_pd(&v2[i]);
 
 		__m256d x2 = _mm256_add_pd(x0, x1);
 
-		_mm256_storeu_pd(&out[i], x2);
+		_mm256_store_pd(&out[i], x2);
 	}
+#pragma ivdep
 	for (; i < (size/2)*2; i+=2) {
-		__m128d x0 = _mm_loadu_pd(&v1[i]);
-		__m128d x1 = _mm_loadu_pd(&v2[i]);
+		__m128d x0 = _mm_load_pd(&v1[i]);
+		__m128d x1 = _mm_load_pd(&v2[i]);
 
 		__m128d x2 = _mm_add_pd(x0, x1);
 
-		_mm_storeu_pd(&out[i], x2);
+		_mm_store_pd(&out[i], x2);
 	}
+#pragma ivdep
 	for (; i < size; i++) {
 		out[i] = v1[i] + v2[i];
 	}
 }
 
+void mulVectors(const double* v1,const double* v2, double* out, int size) {
+	int i;
+#pragma ivdep
+	for (i = 0; i < (size / 4) * 4; i += 4) {
+		__m256d x0 = _mm256_load_pd(&v1[i]);
+		__m256d x1 = _mm256_load_pd(&v2[i]);
+
+		__m256d x2 = _mm256_mul_pd(x0, x1);
+
+		_mm256_store_pd(&out[i], x2);
+	}
+#pragma ivdep
+	for (; i < (size / 2) * 2; i += 2) {
+		__m128d x0 = _mm_load_pd(&v1[i]);
+		__m128d x1 = _mm_load_pd(&v2[i]);
+
+		__m128d x2 = _mm_mul_pd(x0, x1);
+
+		_mm_store_pd(&out[i], x2);
+	}
+#pragma ivdep
+	for (; i < size; i++) {
+		out[i] = v1[i] * v2[i];
+	}
+}
+
+double scalarMult(const double* v1, const double* v2, int size) {
+
+}
+
 int main() {
 	double* a, * b, * c;
 
-	a = new double[VEC_SIZE];
-	b = new double[VEC_SIZE];
-	c = new double[VEC_SIZE];
+	a = (double*)_mm_malloc(VEC_SIZE * sizeof(double), 32);
+	b = (double*)_mm_malloc(VEC_SIZE * sizeof(double), 32);
+	c = (double*)_mm_malloc(VEC_SIZE * sizeof(double), 32);
 	
 	auto start = chrono::high_resolution_clock::now();
 	addVectors(a, b, c, VEC_SIZE);
@@ -45,8 +78,7 @@ int main() {
 
 	cout << res.count();
 
-	delete[] a;
-	delete[] b;
-	delete[] c;
-
+	_mm_free(a);
+	_mm_free(b);
+	_mm_free(c);
 }
